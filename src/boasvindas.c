3b760693a4285e65b3cdc8bd34f6d715f1d0b7b8
Yesenia Salas <yesisalas1804@gmail.com>
2024-10-22 17:30:46 -0300

    Linguagem C

---
+++ src/boasvindas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+// Adicionar um pequeno cabeçalho
+printf("---------------------Programa mensagem---------");
+printf("\n Seja Bem-Vindo ao nosso programa\n");
+printf("-------------------fim------------------------\n");
+
+return 0;
+
+}
